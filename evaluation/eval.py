5b20dcdd58c32b8079a94e2d3afc20f004d2d4cd
PSRahul <rahulpsr95@gmail.com>
2022-10-12 10:24:43 +0200

    made IoU calculation only at 0.5

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -16,7 +16,26 @@ annType = annType[1]      #specify type here
 cocoGt=COCO(annFile)
 cocoDt=cocoGt.loadRes(resFile)
 
-cocoEval = COCOeval(cocoGt,cocoDt,annType)
+class_name=[]
+class_id=[]
+print("Classes : All classes")
+cocoEval = COCOeval(cocoGt, cocoDt, annType)
+cocoEval.params.iouThrs = [0.5]
 cocoEval.evaluate()
 cocoEval.accumulate()
 cocoEval.summarize()
+for index in range(len(cocoGt.cats)):
+    cat = cocoGt.cats[index]
+    class_name.append(cat["name"])
+    class_id.append(cat["id"])
+
+for i,cat_id in enumerate(class_id,0):
+    print("Classes : ",class_name[i])
+
+    cocoEval = COCOeval(cocoGt, cocoDt, annType)
+    cocoEval.params.catIds=cat_id
+    cocoEval.params.iouThrs = [0.5]
+
+    cocoEval.evaluate()
+    cocoEval.accumulate()
+    cocoEval.summarize()
