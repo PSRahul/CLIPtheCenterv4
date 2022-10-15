3999da90162057070454b0b75cd953d67f6ca5fc
PSRahul <rahulpsr95@gmail.com>
2022-10-15 12:27:48 +0200

    1. Class assignment works on PASCAL_3_2_10

---
+++ evaluation/eval_clip.py
@@ -0,0 +1,42 @@
+annFile = "/home/psrahul/MasterThesis/datasets/PASCAL_3_2_10/base_classes/train/coco/labels.json"
+resFile = "/home/psrahul/MasterThesis/repo/Phase7/CenterCLIP_Outputs/exp/ctdet/PASCAL_3_2_10_CA_res_18_cliptest_4/results.json"
+clip_embedding_root= "/home/psrahul/MasterThesis/datasets/BBoxGroundtruths/PASCAL_15_5/train/"
+
+
+import matplotlib.pyplot as plt
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from evaluation.utils import get_class_embeddings,proces_dataset_class
+
+annType = ['segm','bbox','keypoints']
+annType = annType[1]      #specify type here
+
+cocoGt=COCO(annFile)
+cocoDt=cocoGt.loadRes(resFile)
+
+gt_clip_embeddings,class_id_list,class_name_list=get_class_embeddings(clip_embedding_root=clip_embedding_root,dataset=cocoGt)
+cocoDt=proces_dataset_class(cocoDt,gt_clip_embeddings,class_id_list,class_name_list)
+
+class_name=[]
+class_id=[]
+print("Classes : All classes")
+cocoEval = COCOeval(cocoGt, cocoDt, annType)
+cocoEval.params.iouThrs = [0.5]
+cocoEval.evaluate()
+cocoEval.accumulate()
+cocoEval.summarize()
+for index in range(len(cocoGt.cats)):
+    cat = cocoGt.cats[index]
+    class_name.append(cat["name"])
+    class_id.append(cat["id"])
+
+for i,cat_id in enumerate(class_id,0):
+    print("Classes : ",class_name[i])
+
+    cocoEval = COCOeval(cocoGt, cocoDt, annType)
+    cocoEval.params.catIds=cat_id
+    cocoEval.params.iouThrs = [0.5]
+
+    cocoEval.evaluate()
+    cocoEval.accumulate()
+    cocoEval.summarize()
