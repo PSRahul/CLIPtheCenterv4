9e65dc87a1509388cf9fea05b58fde201708d43e
PSRahul <rahulpsr95@gmail.com>
2022-10-18 10:41:51 +0200

    1.remove CenterCrop from clip_preprocess

--- src/lib/models/clip/clip_model.py
+++ src/lib/models/clip/clip_model.py
@@ -15,7 +15,7 @@ class CLIPModel(nn.Module):
         self.clip_model =torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
         self.clip_model.fc = nn.Flatten()
         self.clip_preprocess = transforms.Compose([
-                                        transforms.Resize(256),
+                                        transforms.Resize(224),
                                         #transforms.CenterCrop(224),
                                         transforms.ToTensor(),
                                         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
